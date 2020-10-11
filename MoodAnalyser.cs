fbf1ed6669f9a8d77e4fd4dbfaa096ce2531490d
Prathamesh Rajput <rajputprathamesh1998@gmail.com>
2020-10-11 21:58:55 +0530

    [Pratahamesh Rajput] Add. Completed Use Case 3

--- MoodAnalyser.cs
+++ MoodAnalyser.cs
@@ -4,9 +4,22 @@ using System.Text;
 
 namespace MoodAnalyserPackage
 {
+
+    class MoodAnalysisException : Exception
+    {
+        public MoodAnalysisException(string message) : base(message)
+        {
+        }
+    }
+
     public class MoodAnalyser
     {
         string mood;
+        enum InvalidInput
+        {
+            Empty = 1,
+            Null = 2
+        }
 
         public MoodAnalyser(string mood)
         {
@@ -17,15 +30,18 @@ namespace MoodAnalyserPackage
         {
             try
             {
-
-                if (mood.Contains("sad"))
+                if (mood == null)
+                    throw new MoodAnalysisException("No Input Provided");
+                else if (mood.Contains("sad"))
                     return "The mood is Sad";
+                else if (mood == "")
+                    throw new MoodAnalysisException("Empty Input Provided");
                 else
                     return "The mood is Happy";
             }
-            catch(Exception e)
+            catch (MoodAnalysisException e)
             {
-                    return "The mood is Happy";
+                return e.Message;
             }
         }
     }
